bb16b517d10684e4215515feb803ec31b7b57ae0
rachit3014 <rachitkumar3014@gmail.com>
2022-05-24 23:24:52 +0530

    Add project files.

---
+++ Practice_form_nunit_Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Practice_form_nunit_Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practice_form_nunit_Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("384242a8-7120-40d5-acf6-90f20e233cdc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Practice_form_nunit_Testing/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using mrsLibrary;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+
+namespace Practice_form_nunit_Testing
+{
+    
+     class Practice : Library
+    {
+        [SetUp]
+        public void intiliaze()
+        {
+            chrome("https://demoqa.com/text-box");
+
+
+            time(2000);
+        }
+        [Test]
+        public void TextBox()
+        {
+            IJavaScriptExecutor js = (IJavaScriptExecutor)Driver;
+            time(2000);
+            string name = "Rachit Kumar";
+            string email = "rachitkumar3014@gmail.com";
+            string Caddress = "koderma";
+            string Paddress = "patna";
+
+            time(2000);
+            Findid("userName").SendKeys(name);
+            time(2000);
+            Findid("userEmail").SendKeys(email);
+            time(2000);
+            Findid("currentAddress").SendKeys(Caddress);
+            time(2000);
+            Findid("permanentAddress").SendKeys(Paddress);
+
+            time(2000);
+            js.ExecuteScript("window.scrollBy(0,500)");
+            time(2000);
+
+            Findid("submit").Click();
+            time(5000);
+            
+
+            Console.WriteLine("test case ended");
+        }
+        [TearDown]
+        public void endtest()
+        {
+            quit();
+
+        }
+    }
+}
